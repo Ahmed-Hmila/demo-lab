02e110d87e1320e927b8633e44669fb0e1a6b0dc
Ahmed-Hmila <ahmed7hmila@gmail.com>
2025-12-08 16:11:03 +0100

    first commit

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -230,7 +230,7 @@ resource "aws_lb_target_group" "ecs" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
-  target_type = "ip" # Fargate utilise le type IP
+  target_type = "ip" 
 }
 
 # Target Group pour Lambda
@@ -247,7 +247,7 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.ec2.arn # Action par défaut (EC2)
+    target_group_arn = aws_lb_target_group.ec2.arn  
   }
 }
 
